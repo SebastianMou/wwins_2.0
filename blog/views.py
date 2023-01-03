b5dc4585ec6aad92c42eb6a163f5174293dcc19e
SebastianMou <Sebastian_Mou@protonmail.com>
2023-01-02 23:31:09 -0600

    img no delete no

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from .models import PostModel
 from .forms import UserRegisterForm, PostForm, UserUpdateForm, ProfileUpdateForm, PostUpdateForm, CommentForm
