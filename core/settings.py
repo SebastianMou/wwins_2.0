9a5928fecbb245f80fb0799aeb2ae4dccd7239c7
SebastianMou <Sebastian_Mou@protonmail.com>
2023-01-04 23:06:55 -0600

    commit

--- core/settings.py
+++ core/settings.py
@@ -228,10 +228,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ensocio.mx@gmail.com'
-EMAIL_HOST_PASSWORD = 'qtgtupjwhcfiomsl'
+EMAIL_BACKEND = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
