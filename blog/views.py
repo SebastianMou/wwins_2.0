03aa6ddc98f1c3d5b88bedb87340b48220e77498
SebastianMou <Sebastian_Mou@protonmail.com>
2023-01-22 20:40:38 -0600

    first commit

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.core.mail import send_mail
-from django.core.paginator import Paginator
 
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
